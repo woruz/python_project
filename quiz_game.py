1f7c00a5cfca0f37f661d7c4fba0a1b0f7cefd7c
sabil <sabil.danish1997@gmail.com>
2024-03-09 12:26:03 +0530

    python begineer projects

---
+++ quiz_game.py
@@ -0,0 +1,36 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play?")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! lets play:")
+
+answer = input("What does CPU stand for")
+
+if answer == "central processing unit":
+    print('Correct!')
+else:
+    print("Incorrect")
+
+answer = input("What does GPU stand for")
+
+if answer == "graphics processing unit":
+    print('Correct!')
+else:
+    print("Incorrect")
+
+answer = input("What does RAM stand for")
+
+if answer == "random access memory":
+    print('Correct!')
+else:
+    print("Incorrect")
+
+answer = input("What does PSU stand for")
+
+if answer == "power supply":
+    print('Correct!')
+else:
+    print("Incorrect")
